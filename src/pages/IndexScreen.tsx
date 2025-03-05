672a62b5057c34ac95a2a48a56041369e108c24b
Luux2 <293428@via.dk>
2025-03-05 20:46:13 +0100

    Ændret titlen

--- src/pages/IndexScreen.tsx
+++ src/pages/IndexScreen.tsx
@@ -29,7 +29,7 @@ const IndexScreen = () => {
     return (
         <div className="flex items-center justify-center h-screen">
             <div className="flex flex-col space-y-10 text-center w-64">
-                <h1 className="text-4xl font-semibold">Lunar træningsapp</h1>
+                <h1 className="text-4xl font-semibold">PN træningsapp</h1>
                 <Button className="bg-green-500 rounded-3xl py-2 font-semibold" onClick={handleCreateMatches}>Opret
                     runde</Button>
                 <Button className="bg-orange-500 rounded-3xl py-2 font-semibold" onClick={handleAllRounds}>Se
